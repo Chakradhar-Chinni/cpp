b7e3ffd17992a8554bb17b5b9ffd81e5add1bd89
Chakradhar-Chinni <49102279+Chakradhar-Chinni@users.noreply.github.com>
2022-03-17 13:28:26 +0530

    Data Types

--- codes.cpp
+++ codes.cpp
@@ -7,3 +7,22 @@ int main()
 	cout << "Hello World" << endl;
 	return 0;
 }
+
+`````````Data Types
+#include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+	int n1 = 10;
+	string name="Snow";   
+	char grade = 'A';
+	float n2 = 5.7135;
+	double n3 = 5.700001;
+	bool n4 = true;
+	bool n5 = false;
+	
+	cout << n1 << name << endl;
+	cout << n4;
+	cout << grade;
+ }
